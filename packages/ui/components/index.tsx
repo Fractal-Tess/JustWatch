121dbb4ed7dde7471096953b58c44607dab73919
Fractal-Tess <vgfractal@gmail.com>
2022-10-08 20:05:54 +0300

    nested exports

---
+++ packages/ui/components/index.tsx
@@ -0,0 +1,4 @@
+export * from './theme/ThemeToggle';
+export * from './animation/AnimateInView';
+export * from './anchor/Anchor';
+export * from './search/Search';
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,3 +1,1 @@
-export * from './components/theme/ThemeToggle';
-export * from './components/animation/AnimateInView';
-export * from './components/anchor/Anchor';
+export * from './components';

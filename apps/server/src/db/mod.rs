d97456a35143692c4ba1f1deeb2be26d3e2571f3
Fractal-Tess <vgfractal@gmail.com>
2022-10-19 22:00:21 +0300

    removed unused imports

--- apps/server/src/db/mod.rs
+++ apps/server/src/db/mod.rs
@@ -3,9 +3,5 @@ mod migrations;
 mod models;
 mod rusqlite;
 
-use rocket::fairing::AdHoc;
-use rocket::{Build, Rocket};
-use rocket_sync_db_pools::rusqlite::params;
-
 pub use self::migrations::migrations;
 pub use self::rusqlite::DB;
